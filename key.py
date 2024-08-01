45fd13cc605eb905063d5e6330be4afb3ca6e96b
MaxYu <101554488+MaxYu2@users.noreply.github.com>
2024-08-01 09:05:49 +0800

    Update key.py

--- key.py
+++ key.py
@@ -6,6 +6,6 @@ WEAPI = os.getenv('WEAPI')
 WEUSERID = os.getenv('WEUSERID')
 
 if token == None:
-	token = ''
+	token = 'eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVkIjoxNzIyNDczNDg1MTg1LCJ1c2VySWQiOjE1MDA0NDk0fQ.Td26GEbjUGdASwHl6spwogXlyI5pLJPUojJinyyqhG0'
 if devCode == None:
-	devCode = ''
+	devCode = '3e1971c0-cf60-4d31-a415-175260f8b224'
